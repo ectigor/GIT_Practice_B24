f8a10655651eba30faee719989718d79c11b3a0e
ectigor <ectigor@yahoo.com>
2021-10-03 14:57:26 -1000

    update second file again

--- src/setup/Second.java
+++ src/setup/Second.java
@@ -2,15 +2,17 @@ package setup;
 
 public class Second {
     public void m(int a ){
+        System.out.println("one");
 
     }
     public void m(String s){
+        System.out.println("2");
 
 
 
 
      }
      public void m(){
-
+         System.out.println("3");
      }
 }
