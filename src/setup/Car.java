51a8ab8b696b45f89d6d476486128ddf447f75f1
ectigor <ectigor@yahoo.com>
2021-10-04 07:34:45 -1000

    made car in master

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -2,17 +2,17 @@ package setup;
 
 public class Car {
 
-<<<<<<< HEAD
+
     // new car
     // for james
     //
     // nanana
-=======
+
 
     // this is our car
 
     //more text
 
     // more
->>>>>>> james
+
 }
